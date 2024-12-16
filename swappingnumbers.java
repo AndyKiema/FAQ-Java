b28bc3fef820e4d4461eae8c43a271849da81089
Andrew Kiema <andrewkiema94@gmail.com>
2024-12-16 15:06:28 +0300

    Update files

--- swappingnumbers.java
+++ swappingnumbers.java
@@ -1,7 +1,7 @@
 public class swappingnumbers {
     public static void main(String[] args) {
         int a =10;
-        int b=20;
+        int b =20;
         System.out.println("The value of a is "+a);
         System.out.println("The value of b is "+b);
         int x;
