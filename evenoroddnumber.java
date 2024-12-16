9b73f3a4752c2b24ac17868457d3def2ffe2b2a9
Andrew Kiema <andrewkiema94@gmail.com>
2024-12-16 14:59:04 +0300

    Add Even-Odd number checker

--- evenoroddnumber.java
+++ evenoroddnumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class evenorprimenumber {
+public class evenoroddnumber {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int number=sc.nextInt();
